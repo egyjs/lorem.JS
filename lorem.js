a15cd7875ea7773cbb3f4308d56fcdd02a6a5225
Abdulrahman elzahaby <el3zahaby@gmail.com>
2019-11-12 13:40:51 +0300

    Add files via upload

--- lorem.js
+++ lorem.js
@@ -24,6 +24,7 @@
       return this;
     },
     displayLorem: function(length) {
+        console.log(this.config.length);
         if (this.config.length === 'rand')  {
             var arrrand= ['one','two','three','full']
             this.config.length = arrrand[Math.floor(Math.random() * arrrand.length)];
@@ -34,11 +35,16 @@
       if (this.config.length === 'two')   { $(this.elem).html("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."); }
       if (this.config.length === 'three') { $(this.elem).html("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."); }
       if (this.config.length === 'full')  { $(this.elem).html("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."); }
+      if ( parseInt(this.config.length) > 0)  {
+          var txt =  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+          var res = txt.slice(0, this.config.length);
+          $(this.elem).html(res);
+      }
 
 
 
     }
-  }
+  };
 
   Lorem.defaults = Lorem.prototype.defaults;
 
